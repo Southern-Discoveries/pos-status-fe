680f7fca26a119c9b9ca199e3db2ac86edb1c93b
kafierence <vannam34760@gmail.com>
2023-11-08 17:38:08 +0700

    [fix] lint

--- components/Menu/DotMenuMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import DotIcon from '@/public/assets/icons/line/dot.svg';
-interface IProps {}
-const DotMenuMessage = () => {
-  return <div>DotMenuMessage</div>;
-};
-
-export default DotMenuMessage;
