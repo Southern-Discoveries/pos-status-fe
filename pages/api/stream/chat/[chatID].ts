9d222961ff208aa3bc80603f3829990f8d71fb1f
kafierence <vannam34760@gmail.com>
2023-11-08 18:13:38 +0700

    [fix] lint

--- pages/api/stream/chat/[chatID].ts
+++ pages/api/stream/chat/[chatID].ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { NextApiRequest, NextApiResponse } from 'next';
-const AI_SERVICE_URL =
-  process.env.NEXT_PUBLIC_AI_SERVICE_URL || 'http://127.0.0.1:8000';
+const AI_SERVICE_URL = process.env.NEXT_PUBLIC_AI_SERVICE_URL;
 
 const streamForward = async (
   chatID: any,
